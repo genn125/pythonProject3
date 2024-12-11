f426417f2e00bbc2518a97010ebe2f39693b2f17
genn125 <genn125@gmail.com>
2024-12-11 12:38:29 +0300

    111

---
+++ www3.py
@@ -0,0 +1,12 @@
+def single_root_words (*other_words, root_word = 'ГРИБ', ):
+
+if root_word = other_words:
+
+    # print(f'{other_words} {root_word}')
+
+
+
+
+
+
+single_root_words('грибник', 'грибок', 'грибной', 'грибница','грибочек')
